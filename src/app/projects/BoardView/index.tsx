7aa178086c4c582c04f676ee4a858ca56f0d56c5
Mohammad-dn <mohammaddehghani2677@gmail.com>
2024-10-21 15:23:58 +0330

    add some code

--- src/app/projects/BoardView/index.tsx
+++ src/app/projects/BoardView/index.tsx
@@ -3,8 +3,10 @@ import {
   useGetTasksQuery,
   useUpdateTaskStatusMutation,
 } from "@/src/state/api/api";
-import { DndProvider, useDrop } from "react-dnd";
+import { EllipsisVertical, Plus } from "lucide-react";
+import { DndProvider, useDrag, useDrop } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
+import { format } from "date-fns";
 
 type BoardProps = {
   id: string;
@@ -84,10 +86,68 @@ const TaskColumn = ({
             backgroundColor: statusColor[status],
           }}
         />
-        <div className="jstify-between flex w-full items-center rounded-e-lg bg-white px-5 py-4"></div>
+        <div className="jstify-between flex w-full items-center rounded-e-lg bg-white px-5 py-4 dark:bg-dark-secondary">
+          <h3 className="flex items-center text-lg font-semibold dark:text-white">
+            {status}{" "}
+            <span
+              className="ml-2 inline-block rounded-full bg-gray-200 p-1 text-center text-sm leading-none dark:bg-dark-tertiary"
+              style={{
+                width: "1.5rem",
+                height: "1.5rem",
+              }}
+            >
+              {taskCount}
+            </span>
+          </h3>
+          <div className="flex items-center gap-1">
+            <button className="dark:text-nutar50 flex h-6 w-5 items-center justify-center dark:text-neutral-500">
+              <EllipsisVertical size={26} />
+            </button>
+            <button
+              className="flex h-6 w-6 items-center justify-center rounded bg-gray-200 dark:bg-dark-tertiary dark:text-white"
+              onClick={() => setisModalNewTaskOpen(true)}
+            >
+              <Plus size={16} />
+            </button>
+          </div>
+        </div>
       </div>
+      {tasks
+        ?.filter((task) => task.status === status)
+        .map((task) => <Task key={task?.id} task={task} />)}
     </div>
   );
 };
+
+// type TaskProps{
+//   task:TaskTyppe
+// }
+
+const Task = ({ task }: TaskProps) => {
+  const [{ isDragging }, drop] = useDrag(() => ({
+    type: "task",
+    item: { id: task.id },
+    collect: (monitor: any) => ({
+      isDragging: !!monitor.isDragging(),
+    }),
+  }));
+  const taskTagsSplit = task.tags ? task.tags.split(",") : [];
+  const formattedStartDate = task.startDate
+    ? format(new Date(task.startDate), "P")
+    : "";
+  const formattedDueDate = task.dueDate
+    ? format(new Date(task.dueDate), "P")
+    : "";
+  const numberOfComments = (task.comments && task?.comments?.length) || 0;
+
+  const priorityTags = ({ priority }: { priority: TaskType["priority"] }) => (
+    <div
+      className={`rounded-full px-2 py-1 text-xs font-semibold ${priority === "Urgent" ? "bg-red-200 text-red-700" : priority === "High" ? "bg-yellow-200 text-yellow-700" : priority === "Low"}`}
+    >
+      {priority}
+    </div>
+  );
+};
+
 
 export default BoardView;
